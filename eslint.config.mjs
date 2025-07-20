fe656d4828c1a8218810f65f983f4f015d12d1f2
Masaru Nemoto <16267290+nemolize@users.noreply.github.com>
2025-07-20 16:57:29 +0900

    chore: add Prettier and ESLint configuration
    - Created .prettierignore to exclude pnpm-lock.yaml from formatting.
    - Added .prettierrc with Tailwind CSS plugin for Prettier.
    - Introduced ESLint configuration with support for Next.js and TypeScript, including unused imports handling.

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import gitignore from "eslint-config-flat-gitignore";
+import unusedImports from "eslint-plugin-unused-imports";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  gitignore(),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
