a9b73979a58bc0b3c39f01f4be4fd3855d1421c7
Masaru Nemoto <16267290+nemolize@users.noreply.github.com>
2025-11-19 22:54:40 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
+
+ 
